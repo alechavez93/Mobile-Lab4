82ac5e6a1075712cf89c42bc5c8fc492bee06772
yotoo2920 <d.orta.santana@ufl.edu>
2017-03-21 01:40:14 -0400

    Map updated in dashboard

--- mqtt_node/public/js/dashboard.js
+++ mqtt_node/public/js/dashboard.js
@@ -1,17 +1,27 @@
 /**
  * Created by yotoo2920 on 3/17/2017.
  */
-function initMap() {
-    var uluru = {lat: -20.363, lng: 131.044};
-    var map = new google.maps.Map(canvas, {
-        zoom: 4,
-        center: uluru
-    });
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
-}
 
-var canvas = document.getElementById("grid");
-initMap();
+function initMap() {
+
+    // Gainesville FL (UF)
+    var uf = {lat: 29.6436325, lng: -82.3571189};
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 17,
+        center: uf
+    });
+
+    // for (var city in citymap) {
+        // Add the circle for this city to the map.
+        var cityCircle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: uf,
+            radius: 0.8 * 100
+        });
+    // }
+}
