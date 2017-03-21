47ed602f0c2b17791bc95cb4b54e579bf16481f7
vladimirpc1985 <vladimirpc1985@gmail.com>
2017-03-21 05:20:51 -0400

    Vladimir first commit. It has an error, and I did not run it.

---
+++ BeaconTracker/app/src/main/java/com/example/ale/beacontracker/Utility/ScannerBeacom.java
@@ -0,0 +1,127 @@
+package com.example.ale.beacontracker.Utility;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+
+import com.example.ale.beacontracker.MainActivity;
+
+
+public class ScannerBeacon
+{
+    private MainActivity mainActivity;
+    private long scanPeriod;
+    private BluetoothAdapter mBluetoothAdapter;
+    private Handler mHandler;
+    private boolean mScanning;
+
+    private int signalStrength;
+
+
+
+    // Constructor
+    public ScannerBeacon(MainActivity mainActivity, long scanPeriod, int signalStrength)
+    {
+        this.mainActivity = mainActivity;
+        this.scanPeriod = scanPeriod;
+        this.signalStrength = signalStrength;
+
+        mHandler = new Handler();
+
+        final BluetoothManager bluetoothManager = (BluetoothManager) mainActivity.getSystemService(Context.BLUETOOTH_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            mBluetoothAdapter = bluetoothManager.getAdapter();
+        } else {
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        }
+
+    }
+
+
+    public void start()
+    {
+        if (!checkBluetooth()) {
+            requestUserBluetooth();
+            mainActivity.stopScan();
+        }
+        else {
+            scanLeDevice(true);
+        }
+    }
+
+    public boolean checkBluetooth() {
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled())
+            return false;
+        else
+            return true;
+    }
+
+    public void requestUserBluetooth() {
+        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        mainActivity.startActivityForResult(enableBtIntent, MainActivity.REQUEST_ENABLE_BT);
+    }
+
+    public void stop() {
+        scanLeDevice(false);
+    }
+
+    // If you want to scan for only specific types of peripherals,
+    // you can instead call startLeScan(UUID[], BluetoothAdapter.LeScanCallback),
+    // providing an array of UUID objects that specify the GATT services your app supports.
+    private void scanLeDevice(final boolean enable) {
+        if (enable && !mScanning) {
+            Utils.toast(mainActivity.getApplicationContext(), "Starting BLE scan...");
+
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Utils.toast(mainActivity.getApplicationContext(), "Stopping BLE scan...");
+
+                    mScanning = false;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                        mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                    }
+
+                    mainActivity.stopScan();
+                }
+            }, scanPeriod);
+
+            mScanning = true;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                mBluetoothAdapter.startLeScan(mLeScanCallback);
+            }
+        }
+        else {
+            mScanning = false;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                mBluetoothAdapter.stopLeScan(mLeScanCallback);
+            }
+        }
+    }
+
+
+    // Device scan callback.
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+                @Override
+                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+
+                    final int new_rssi = rssi;
+                    if (rssi > signalStrength) {
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                mainActivity.addDevice(device, new_rssi);
+                            }
+                        });
+                    }
+                }
+            };
+}
---
+++ BeaconTracker/app/src/main/java/com/example/ale/beacontracker/Utility/Utils.java
@@ -0,0 +1,16 @@
+package com.example.ale.beacontracker.Utility;
+
+import android.content.Context;
+import android.widget.Toast;
+import android.view.Gravity;
+
+
+
+public class Utils {
+
+    public static void toast(Context context, String string) {
+        Toast toast = Toast.makeText(context, string, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER | Gravity.BOTTOM, 0, 0);
+        toast.show();
+    }
+}
